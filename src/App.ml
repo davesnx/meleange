5c7cc3f4ce694b07b3027fce6a7e2e2eb2e20583
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-06-04 11:57:32 +0200

    Use html parsing instead of string replacement

--- src/App.ml
+++ src/App.ml
@@ -2,8 +2,8 @@ type line = { start : point; end_ : point; thickness : int option }
 and point = { x : int; y : int }
 
 let line_to_string line =
-  Printf.sprintf "Line from (%d, %d) to (%d, %d)"
-  line.start.x line.start.y line.end_.x line.end_.y
+  Printf.sprintf "Line from (%d, %d) to (%d, %d)" line.start.x line.start.y
+    line.end_.x line.end_.y
 
 module Decode = struct
   let point json =
@@ -20,9 +20,11 @@ module Decode = struct
 end
 
 let () =
-  let data = {| { "start": { "x": 1, "y": -4 }, "end": { "x": 5, "y": 8 } } |} in
+  let data =
+    {| { "start": { "x": 1, "y": -4 }, "end": { "x": 5, "y": 8 } } |}
+  in
   let line = data |> Json.parseOrRaise |> Decode.line in
-  let total = Belt.List.reduce [1; 2; 3] 0 (+) in
+  let total = Belt.List.reduce [ 1; 2; 3 ] 0 ( + ) in
   let message = Lib.print total in
   Js.Console.log message;
-  Js.Console.log (line_to_string line);
+  Js.Console.log (line_to_string line)
